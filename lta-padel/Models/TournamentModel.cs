7c8ad5c912dc100922c42fcd9ae362e8bc48b64b
Marcos Gimeno <director@compuglobalhypermeganet.ltd>
2019-10-31 19:39:41 +0000

    Minor improvement

--- lta-padel/Models/TournamentModel.cs
+++ lta-padel/Models/TournamentModel.cs
@@ -6,6 +6,6 @@ namespace lta_padel.Models
     public class TournamentModel
     {
         public string Name { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; } = null;
     }
 }
